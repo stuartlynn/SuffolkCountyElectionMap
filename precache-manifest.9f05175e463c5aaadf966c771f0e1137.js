0a42611cda89dfb27fb9df7ec4a4e416a6ff8e14
Stuart <stuart.lynn@gmail.com>
2019-02-26 21:44:25 -0500

    Updates

---
+++ precache-manifest.9f05175e463c5aaadf966c771f0e1137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "04f582c9f1283f659270",
+    "url": "/static/js/main.04f582c9.chunk.js"
+  },
+  {
+    "revision": "47fb228bee98d3104e25",
+    "url": "/static/js/2.47fb228b.chunk.js"
+  },
+  {
+    "revision": "04f582c9f1283f659270",
+    "url": "/static/css/main.494c1bbe.chunk.css"
+  },
+  {
+    "revision": "47fb228bee98d3104e25",
+    "url": "/static/css/2.f881401b.chunk.css"
+  },
+  {
+    "revision": "3bf04f668b8aea76c96a1681c3e3b7eb",
+    "url": "/index.html"
+  }
+];
